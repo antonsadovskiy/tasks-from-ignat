d655802b687d6c911bc0db48ce1a781a977b7c7a
antonsadovskiy <antonsadovskiy6@gmail.com>
2023-02-20 21:49:26 +0300

    changed onChangeHandler

--- src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -28,7 +28,7 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
         // задачка на написание онченджа
-        if(onChangeChecked){
+        if (onChangeChecked) {
             onChangeChecked(e.currentTarget.checked)
         }
     }
